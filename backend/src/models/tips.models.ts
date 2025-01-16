ac6c9ecbd17e142e2e370b9e07ad69faec836818
Kostadin Devedzhiev <kostadin.g.devedzhiev@gmail.com>
2025-01-16 16:57:33 -0500

    add tips models

--- backend/src/models/tips.models.ts
+++ backend/src/models/tips.models.ts
@@ -1,6 +1,26 @@
+import { Types } from "mongoose";
+
 export type TipsType = 'matchup' | 'synergy';
 
 export interface Tip {
   title: string,
   description: string,
+}
+
+export interface TipsResponse {
+  tips: Tip[]
+}
+
+export interface ITip extends Document {
+  title: string,
+  description: string,
+  _id: Types.ObjectId;  // Updated to use ObjectId type
+}
+
+export interface ITips extends Document {
+  myChampion: string,
+  otherChampion: string,
+  tipsType: TipsType,
+  tips: ITip[],
+  _id: Types.ObjectId;  // Updated to use ObjectId type
 }
