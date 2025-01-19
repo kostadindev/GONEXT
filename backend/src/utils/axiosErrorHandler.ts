b392346b0f98439127e29e3f46193f76ed80cc3f
Kostadin Devedzhiev <kostadin.g.devedzhiev@gmail.com>
2025-01-18 20:22:53 -0500

    improve errror message

--- backend/src/utils/axiosErrorHandler.ts
+++ backend/src/utils/axiosErrorHandler.ts
@@ -5,13 +5,15 @@ import { AxiosError } from "axios";
  * @param {AxiosError} error - The error object from axios.
  */
 export const handleAxiosError = (error: AxiosError): void => {
-  const { response, request, message } = error;
+  const { response, request, message, config } = error;
 
   if (response) {
-    console.error(`HTTP error: ${response.status} - ${response.statusText}`);
+    console.error(
+      `HTTP error: ${response.status} - ${response.statusText} (URL: ${config?.url})`
+    );
     console.error("Response data:", JSON.stringify(response.data));
   } else if (request) {
-    console.error("No response received from the API.");
+    console.error(`No response received from the API (URL: ${config?.url}).`);
     console.error("Request details:", request);
   } else {
     console.error(`An error occurred: ${message}`);
