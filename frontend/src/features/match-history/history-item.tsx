edf67eeac35fdd8a11ac12455e124b18aa8b267d
Kostadin Devedzhiev <kostadin.g.devedzhiev@gmail.com>
2025-01-23 20:57:00 -0500

    add tooltips over items

--- frontend/src/features/match-history/history-item.tsx
+++ frontend/src/features/match-history/history-item.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Card, Typography, Badge } from "antd";
+import { Avatar, Card, Badge, Tooltip } from "antd";
 import { formatDistanceToNow } from "date-fns";
 import { formatGameDurationFromMs } from "../../libs/general/utilities";
 import { Summoner, GameHistory } from "../../libs/league/league-types";
@@ -14,24 +14,28 @@ const MiniSummonerDisplay: React.FC<{ summoner: Summoner }> = ({
   summoner,
 }) => (
   <div key={summoner?.summonerName} className="flex items-center w-full">
-    <Avatar
-      src={getChampionIconSrc(summoner?.championImageId)}
-      alt={summoner?.summonerSpell1Name}
-      size={18}
-      shape="square"
-    />
+    <Tooltip title={`Champion: ${summoner?.championImageId || "Unknown"}`}>
+      <Avatar
+        src={getChampionIconSrc(summoner?.championImageId)}
+        alt={summoner?.summonerSpell1Name}
+        size={18}
+        shape="square"
+      />
+    </Tooltip>
     <div className="ml-2 flex-1 min-w-0">
-      <span
-        className="block truncate"
-        style={{
-          whiteSpace: "nowrap",
-          overflow: "hidden",
-          textOverflow: "ellipsis",
-          color: "gray",
-        }}
-      >
-        {summoner.summonerName}
-      </span>
+      <Tooltip title={summoner.summonerName}>
+        <span
+          className="block truncate"
+          style={{
+            whiteSpace: "nowrap",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            color: "gray",
+          }}
+        >
+          {summoner.summonerName}
+        </span>
+      </Tooltip>
     </div>
   </div>
 );
@@ -61,43 +65,75 @@ const ParticipantInfo: React.FC<{ game: GameHistory; kda: string }> = ({
     <div className="w-[200px] flex gap-4">
       <div className="flex gap-1">
         <Badge>
-          <Avatar
-            src={getChampionIconSrc(game?.participant?.championImageId)}
-            size={50}
-          />
+          <Tooltip
+            title={`Champion: ${
+              game?.participant?.championImageId || "Unknown"
+            }`}
+          >
+            <Avatar
+              src={getChampionIconSrc(game?.participant?.championImageId)}
+              size={50}
+            />
+          </Tooltip>
         </Badge>
         <div className="flex flex-col">
-          <Avatar
-            src={getSummonerSpellIconSrc(game?.participant?.summonerSpell1Name)}
-            alt={game?.participant?.summonerSpell1Name}
-            size={SMALL_ICON_SIZE}
-          />
-          <Avatar
-            src={getSummonerSpellIconSrc(game?.participant?.summonerSpell2Name)}
-            alt={game?.participant?.summonerSpell2Name}
-            size={SMALL_ICON_SIZE}
-          />
+          <Tooltip
+            title={`Summoner Spell 1: ${
+              game?.participant?.summonerSpell1Name || "Unknown"
+            }`}
+          >
+            <Avatar
+              src={getSummonerSpellIconSrc(
+                game?.participant?.summonerSpell1Name
+              )}
+              alt={game?.participant?.summonerSpell1Name}
+              size={SMALL_ICON_SIZE}
+            />
+          </Tooltip>
+          <Tooltip
+            title={`Summoner Spell 2: ${
+              game?.participant?.summonerSpell2Name || "Unknown"
+            }`}
+          >
+            <Avatar
+              src={getSummonerSpellIconSrc(
+                game?.participant?.summonerSpell2Name
+              )}
+              alt={game?.participant?.summonerSpell2Name}
+              size={SMALL_ICON_SIZE}
+            />
+          </Tooltip>
         </div>
       </div>
       <div className="min-w-[80]">
-        <div className="text-base flex justify-center items-center">
-          <span>{game?.participant?.kills}</span>/
-          <span className="text-red-500">{game?.participant?.deaths}</span>/
-          <span>{game?.participant?.assists}</span>
-        </div>
-        <div className="flex justify-center text-gray-500">{kda}</div>
+        <Tooltip title="Kills/Deaths/Assists">
+          <div className="text-base flex justify-center items-center">
+            <span>{game?.participant?.kills}</span>/
+            <span className="text-red-500">{game?.participant?.deaths}</span>/
+            <span>{game?.participant?.assists}</span>
+          </div>
+        </Tooltip>
+        <Tooltip title="(Kills + Assists) / Deaths">
+          <div className="flex justify-center text-gray-500">{kda}</div>
+        </Tooltip>
       </div>
     </div>
 
     <div className="flex gap-0.5 pt-2">
       {[0, 1, 2, 3, 4, 5].map((item) => (
-        <Avatar
+        <Tooltip
           key={item}
-          src={getItemIconSrcById((game?.participant as any)?.[`item${item}`])}
-          alt={(game?.participant as any)?.[`item${item}`]}
-          size={SMALL_ICON_SIZE}
-          shape="square"
-        />
+          title={`Item ${(game?.participant as any)?.[`item${item}`] || "N/A"}`}
+        >
+          <Avatar
+            src={getItemIconSrcById(
+              (game?.participant as any)?.[`item${item}`]
+            )}
+            alt={(game?.participant as any)?.[`item${item}`]}
+            size={SMALL_ICON_SIZE}
+            shape="square"
+          />
+        </Tooltip>
       ))}
     </div>
   </div>
