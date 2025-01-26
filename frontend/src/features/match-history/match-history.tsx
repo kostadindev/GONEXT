a2ae1a70edfe781338fc4d64c52c194971175de8
Kostadin Devedzhiev <kostadin.g.devedzhiev@gmail.com>
2025-01-26 00:56:41 -0500

    Reapply "fix loading"
    This reverts commit 6d151c1d1e2c3fe8c5064accac5d109cac7a6d58.

--- frontend/src/features/match-history/match-history.tsx
+++ frontend/src/features/match-history/match-history.tsx
@@ -32,14 +32,19 @@ export const MatchHistory: React.FC<MatchHistoryProps> = ({ summoner }) => {
   }, [summoner]);
 
   return (
-    <div className="pt-3 pr-3 rounded-lg flex flex-col">
-      <div className="text-lg font-bold mb-2">
-        {/* {`${summoner.championName}'s Match History`} */}
-      </div>
-      <Spin spinning={isLoading}>
+    <div className="flex flex-col">
+      <div className="flex-1 mb-2 pt-8">
+        {/* Spinner */}
+        {isLoading && (
+          <div className="absolute inset-0 flex items-center justify-center">
+            <Spin />
+          </div>
+        )}
+        {/* Content */}
         <div
-          className="flex-1 overflow-y-auto flex flex-col items-start gap-0" // Added `items-start` and removed spacing
-          style={{ height: "calc(70vh - 50px)" }}
+          className={`flex-1 overflow-y-auto ${
+            isLoading ? "opacity-0" : "opacity-100"
+          } transition-opacity duration-300`}
         >
           {games?.map((game, index) => (
             <div className="max-w-[380px]" key={index}>
@@ -50,7 +55,7 @@ export const MatchHistory: React.FC<MatchHistoryProps> = ({ summoner }) => {
             </div>
           ))}
         </div>
-      </Spin>
+      </div>
     </div>
   );
 };
