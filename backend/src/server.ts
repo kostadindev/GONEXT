ccc321417634430abfdea20a382b433cf2eb89c3
Kostadin Devedzhiev <kostadin.g.devedzhiev@gmail.com>
2025-02-06 12:03:06 -0500

    add neon psql connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,21 +1,42 @@
 import dotenv from "dotenv";
-import http from 'http';
+import http from "http";
 import app from "./app";
 import mongoose from "mongoose";
 import process from "process";
-
-const MONGO_URI = process.env.MONGO_URI as string;
+import { neon } from "@neondatabase/serverless";
 
 dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI as string;
+const sql = neon(process.env.DATABASE_URL);
 const port = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-async function startServer() {
-  await mongoose.connect(MONGO_URI);
-  server.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
-  });
+async function testNeonConnection() {
+  try {
+    const result = await sql`SELECT version()`;
+    console.log("Connected to NeonDB successfully:", result[0].version);
+  } catch (err) {
+    console.error("Error connecting to NeonDB:", err);
+    process.exit(1);
+  }
 }
 
-startServer()
+async function startServer() {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("Connected to MongoDB successfully");
+
+    await testNeonConnection();
+
+    server.listen(port, () => {
+      console.log(`[server]: Server is running at http://localhost:${port}`);
+    });
+  } catch (err) {
+    console.error("Error starting the server:", err);
+    process.exit(1);
+  }
+}
+
+startServer();
