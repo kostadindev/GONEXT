0bd6597e65058653ec299ab52f210b3f36e82556
Kostadin Devedzhiev <kostadin.g.devedzhiev@gmail.com>
2025-02-24 16:33:45 -0500

    redesign about us page

--- frontend/src/features/about-us/about-us.tsx
+++ frontend/src/features/about-us/about-us.tsx
@@ -14,6 +14,7 @@ const { Title, Text, Paragraph } = Typography;
 type TeamMember = {
   name: string;
   title: string;
+  description: string;
   image: string;
   github?: string;
   linkedin?: string;
@@ -26,32 +27,46 @@ const AboutUs: React.FC = () => {
     {
       name: "Kostadin Devedzhiev",
       title: "Founder | ML | API | UI",
+      description:
+        "Kostadin is a visionary leader with expertise in machine learning, API development, and UI design. He drives innovation and excellence in our projects.",
       image:
         "https://media.licdn.com/dms/image/v2/D4E03AQHT-wFQ9xo1sA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1727758296269?e=1743033600&v=beta&t=u7b94xnynKeCumtQDipci8K8p9uiuIgMs_8dO9Qotdo",
       github: "https://github.com/kostadindev",
       linkedin: "https://www.linkedin.com/in/kostadin-dev/",
-      email: "kostadin.g.devedzhiev@gmail.com",
+      email: "kostadin@gonext.lol",
       website: "https://kostadindev.github.io/",
     },
     {
       name: "Ivan Nikolov",
       title: "API | DevOps",
+      description:
+        "Ivan specializes in API development and DevOps practices, ensuring robust, scalable, and secure backend services.",
       image:
-        "https://scontent-lga3-2.xx.fbcdn.net/v/t39.30808-6/463968694_27285182364459185_394874216162696016_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=amd8TTMeACAQ7kNvgFbYleM&_nc_oc=Adhsh2rsAUshCEz8iMZh26cbm3K3qPL6UnuOAMTRBdMX7FqTd1GL6k40-d5fw70Bq6Ju2fmqNLO4_VhbJbLfa2ZM&_nc_zt=23&_nc_ht=scontent-lga3-2.xx&_nc_gid=ARUAB4_vNlIN-aGXlezoqP2&oh=00_AYA2qivcv2ljXqUGH-Tn8Mj7CCe1KV0QL_z3GUd8q7kHEA&oe=67B5D74A",
+        "https://scontent-lga3-2.xx.fbcdn.net/v/t39.30808-6/463968694_27285182364459185_394874216162696016_n.jpg",
       github: "https://github.com/nnivan",
     },
   ];
+
+  const emailStyle = {
+    // color: "#1890ff",
+    fontSize: "16px",
+    marginTop: "10px",
+  };
 
   return (
     <div
       style={{
         display: "flex",
         flexDirection: "column",
-        minHeight: "calc(100vh - 64px)", // Assuming a 64px header height
-        padding: "20px 10%",
+        alignItems: "center",
+        minHeight: "calc(100vh - 64px)",
+        padding: "20px",
       }}
     >
-      <div className="text-center mb-10 mx-auto" style={{ maxWidth: "1100px" }}>
+      <div
+        className="text-center mb-10 mx-auto"
+        style={{ maxWidth: "500px", textAlign: "center" }}
+      >
         <Title level={2} style={{ marginBottom: "16px" }}>
           Meet the Team
         </Title>
@@ -60,82 +75,173 @@ const AboutUs: React.FC = () => {
         </Paragraph>
       </div>
 
-      <Row gutter={[24, 24]} justify="center" style={{ flex: 1 }}>
+      <Row gutter={[24, 24]} style={{ flex: 1, maxWidth: "1000px" }}>
         {teamMembers.map((member, index) => (
-          <Col xs={24} sm={12} md={8} lg={6} key={index}>
+          <Col span={24} key={index}>
             <Card
               hoverable
-              cover={
-                <img
-                  alt={member.name}
-                  src={member.image}
-                  style={{
-                    height: "200px",
-                    width: "200px",
-                    borderRadius: "25%",
-                    objectFit: "contain",
-                    margin: "20px auto",
-                  }}
-                />
-              }
               style={{
                 borderRadius: "8px",
                 overflow: "hidden",
-                textAlign: "center",
+                marginBottom: "20px",
+                padding: "20px",
               }}
             >
-              <Card.Meta
-                title={<Title level={5}>{member.name}</Title>}
-                description={<Text type="secondary">{member.title}</Text>}
-              />
-              <div style={{ marginTop: "10px" }}>
-                {member.website && (
-                  <Tooltip title="Website">
-                    <Button
-                      shape="circle"
-                      type="text"
-                      target="_blank"
-                      href={member.website}
-                      style={{ paddingRight: "0px" }}
-                      icon={<GlobalOutlined />}
-                    />
-                  </Tooltip>
+              <Row gutter={[16, 16]} align="middle">
+                {index % 2 === 0 ? (
+                  <>
+                    <Col xs={24} md={8} style={{ textAlign: "center" }}>
+                      <img
+                        alt={member.name}
+                        src={member.image}
+                        style={{
+                          height: "200px",
+                          width: "200px",
+                          borderRadius: "25%",
+                          objectFit: "contain",
+                        }}
+                      />
+                    </Col>
+                    <Col xs={24} md={16}>
+                      <Title level={5}>{member.name}</Title>
+                      <Text type="secondary">{member.title}</Text>
+                      <Paragraph style={{ marginTop: "10px" }}>
+                        {member.description}
+                      </Paragraph>
+                      {member.email && (
+                        <Paragraph style={emailStyle}>
+                          <a
+                            href={`mailto:${member.email}`}
+                            style={{ fontStyle: "italic"}}
+                          >
+                            {member.email}
+                          </a>
+                        </Paragraph>
+                      )}
+                      <div style={{ marginTop: "10px" }}>
+                        {member.website && (
+                          <Tooltip title="Website">
+                            <Button
+                              shape="circle"
+                              type="text"
+                              target="_blank"
+                              href={member.website}
+                              icon={<GlobalOutlined />}
+                            />
+                          </Tooltip>
+                        )}
+                        {member.email && (
+                          <Tooltip title="Email">
+                            <Button
+                              shape="circle"
+                              type="text"
+                              target="_blank"
+                              href={`mailto:${member.email}`}
+                              icon={<MailFilled />}
+                            />
+                          </Tooltip>
+                        )}
+                        {member.linkedin && (
+                          <Tooltip title="LinkedIn">
+                            <Button
+                              shape="circle"
+                              type="text"
+                              href={member.linkedin}
+                              target="_blank"
+                              icon={<LinkedinFilled />}
+                            />
+                          </Tooltip>
+                        )}
+                        {member.github && (
+                          <Tooltip title="GitHub">
+                            <Button
+                              shape="circle"
+                              type="text"
+                              href={member.github}
+                              target="_blank"
+                              icon={<GithubFilled />}
+                            />
+                          </Tooltip>
+                        )}
+                      </div>
+                    </Col>
+                  </>
+                ) : (
+                  <>
+                    <Col xs={24} md={16}>
+                      <Title level={5}>{member.name}</Title>
+                      <Text type="secondary">{member.title}</Text>
+                      <Paragraph style={{ marginTop: "10px" }}>
+                        {member.description}
+                      </Paragraph>
+                      {member.email && (
+                        <Paragraph style={emailStyle}>
+                          <MailFilled style={{ marginRight: "8px" }} />
+                          {member.email}
+                        </Paragraph>
+                      )}
+                      <div style={{ marginTop: "10px" }}>
+                        {member.website && (
+                          <Tooltip title="Website">
+                            <Button
+                              shape="circle"
+                              type="text"
+                              target="_blank"
+                              href={member.website}
+                              icon={<GlobalOutlined />}
+                            />
+                          </Tooltip>
+                        )}
+                        {member.email && (
+                          <Tooltip title="Email">
+                            <Button
+                              shape="circle"
+                              type="text"
+                              target="_blank"
+                              href={`mailto:${member.email}`}
+                              icon={<MailFilled />}
+                            />
+                          </Tooltip>
+                        )}
+                        {member.linkedin && (
+                          <Tooltip title="LinkedIn">
+                            <Button
+                              shape="circle"
+                              type="text"
+                              href={member.linkedin}
+                              target="_blank"
+                              icon={<LinkedinFilled />}
+                            />
+                          </Tooltip>
+                        )}
+                        {member.github && (
+                          <Tooltip title="GitHub">
+                            <Button
+                              shape="circle"
+                              type="text"
+                              href={member.github}
+                              target="_blank"
+                              icon={<GithubFilled />}
+                            />
+                          </Tooltip>
+                        )}
+                      </div>
+                    </Col>
+                    <Col xs={24} md={8} style={{ textAlign: "center" }}>
+                      <img
+                        alt={member.name}
+                        src={member.image}
+                        style={{
+                          height: "200px",
+                          width: "200px",
+                          borderRadius: "25%",
+                          objectFit: "contain",
+                        }}
+                      />
+                    </Col>
+                  </>
                 )}
-                {member.email && (
-                  <Tooltip title="Email">
-                    <Button
-                      shape="circle"
-                      type="text"
-                      target="_blank"
-                      href={`mailto:${member.email}`}
-                      style={{ paddingRight: "0px" }}
-                      icon={<MailFilled />}
-                    />
-                  </Tooltip>
-                )}
-                {member.linkedin && (
-                  <Tooltip title="LinkedIn">
-                    <Button
-                      shape="circle"
-                      type="text"
-                      href={member.linkedin}
-                      target="_blank"
-                      icon={<LinkedinFilled />}
-                    />
-                  </Tooltip>
-                )}
-                {member.github && (
-                  <Tooltip title="GitHub">
-                    <Button
-                      shape="circle"
-                      type="text"
-                      href={member.github}
-                      target="_blank"
-                      icon={<GithubFilled />}
-                    />
-                  </Tooltip>
-                )}
-              </div>
+              </Row>
             </Card>
           </Col>
         ))}
