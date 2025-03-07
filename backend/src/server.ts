f6426faf40a8608554b9cc9abbd8865f365ea620
nnivan <ivannikolov007@gmail.com>
2025-03-07 22:57:36 +0100

    https for app files

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
-import http from "http";
+import https from "https";
+import fs from "fs";
 import app from "./app";
 import mongoose from "mongoose";
 import process from "process";
@@ -9,13 +10,19 @@ import redis from "./db/redis";
 dotenv.config();
 
 const MONGO_URI = process.env.MONGO_URI as string;
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8443;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
-const server = http.createServer(app);
+// HTTPS options
+const httpsOptions = {
+  key: fs.readFileSync('/etc/letsencrypt/live/gonext.lol/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/gonext.lol/fullchain.pem')
+};
+
+const server = https.createServer(httpsOptions, app);
 
 async function testPostgresConnection() {
   try {
@@ -40,9 +47,9 @@ async function startServer() {
     // Test PostgreSQL connection
     await testPostgresConnection();
 
-    // Start the HTTP server
+    // Start the HTTPS server
     server.listen(port, () => {
-      console.log(`[server]: Server is running at http://gonext.lol:${port}`);
+      console.log(`[server]: Server is running at https://gonext.lol:${port}`);
     });
   } catch (err) {
     console.error("Error starting the server:", err);
